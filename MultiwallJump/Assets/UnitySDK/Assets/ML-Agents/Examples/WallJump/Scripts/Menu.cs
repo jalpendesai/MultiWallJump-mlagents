4f263d5ab43eb40c036a40b112b11e15af8636e2
jalpendesai <djalpen24@gmail.com>
2019-04-19 20:29:05 -0400

    Removed Extra files

---
+++ MultiwallJump/Assets/UnitySDK/Assets/ML-Agents/Examples/WallJump/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Level1()
+    {
+        //  Loads next scene in a queue
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Level2()
+    {
+        //  Loads next scene in a queue
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Level3()
+    {
+        //  Loads next scene in a queue
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+}
